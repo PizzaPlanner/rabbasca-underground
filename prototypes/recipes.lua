f9e5ead5c6b5f7d79eeabd21d997065ee3b53eaf
PizzaPlanner <80210845+PizzaPlanner@users.noreply.github.com>
2026-07-03 22:27:58 +0200

    more forgiving emergency fuel

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -200,15 +200,15 @@ data:extend {
         energy_required = 15,
         ingredients = {
             { type = "item", name = "rabbasca-powerspike", amount = 1 },
-            { type = "item", name = "repair-pack", amount = 20 },
         },
         results = { 
             { type = "item", name = "rabbasca-powershard", amount = 5 },
             { type = "item", name = "rabbasca-powerspike-weak", amount = 1, affected_by_quality = false, always_fresh = true, ignored_by_productivity = 1 },
          },
-        categories = { "crafting" },
+        categories = { "rabbasca-warp-stabilizer" },
         main_product = "rabbasca-powershard",
-        order = "0",
+        subgroup = "rabbasca-events",
+        order = "a[stabilizer]-n",
         hide_from_player_crafting = true,
         allow_productivity = false,
         auto_recycle = false,
@@ -323,7 +323,8 @@ data:extend {
         energy_required = 30,
         ingredients = { { type = "item", name = "rabbasca-powerspike", amount = 1 } },
         results = { 
-            { type = "item", name = "rabbasca-warp-trace", amount = 250, always_fresh = true, affected_by_quality = false }
+            { type = "item", name = "rabbasca-warp-trace", amount = 250, always_fresh = true, affected_by_quality = false },
+            { type = "item", name = "rabbasca-powerspike-weak", amount = 1, affected_by_quality = false, always_fresh = true, ignored_by_productivity = 1 },
         },
         -- main_product = "rabbasca-warp-trace",
         allow_productivity = true,
